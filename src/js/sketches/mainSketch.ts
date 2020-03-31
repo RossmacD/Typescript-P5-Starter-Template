218f6eb50104671e8cbf9138f22c16c2fcd48ce7
Ross MacDonald <42966821+RossmacD@users.noreply.github.com>
2020-03-31 23:47:04 +0100

    Added Kalidescope example

--- src/js/sketches/mainSketch.ts
+++ src/js/sketches/mainSketch.ts
@@ -1,102 +1,50 @@
 import * as p5 from 'p5';
-import Human from '../classes/Human';
+// Symmetry corresponding to the number of reflections. Change the number for different number of reflections 
+const symmetry:number = 8;
 
-// ---------------------------
-// Global Variables
-// ---------------------------
-// let molecules, moleculeKey = [];
-// let collisonNo,checknum = 0;
-// let minRadius = 10, maxRadius = 20, minVelocity = -2, maxVelocity = 2, colWidth, rowHeight;
-// let humans: Array<Human> = [];
-// let humanKey: Array<Array<number>> = [];
-// let collisonNo: number = 0;
-// let checknum:number = 0;
-const minRadius: number = 10;
-const maxRadius: number = 20;
-const numRows: number = 8;
-const numCols: number = 8;
-let colWidth: number;
-let rowHeight: number;
+const angle:number = 360 / symmetry;
 
-// Create an object to hold Variables for gui.
-// GUI cannot get handle on scope otherwise, unless var is used (Undesirable as it is outdated)
-// let guiVars: object = {
-//   numOfMolecules: 1,
-//   numRows: 4,
-//   numCols: 4,
-//   radiusBaseline: 5,
-//   showGrid: true,
-//   render: true,
-//   showTrails: false
-// };
+// let brushSizeSlider:, sizeSlider;
+
 
 const sketch = (p: p5) => {
   /**
    * P5 Setup
    */
   p.setup = () => {
-    console.log('Hi');
-    p.createCanvas(p.windowWidth, p.windowHeight, p.WEBGL);
-    colWidth = p.width / numRows;
-    rowHeight = p.height / numCols;
-    p.background(0);
-    p.stroke(80, 150, 50);
-    p.strokeWeight(1);
-    // Draw Grid
-    for (let x: number = colWidth; x < p.width; x += colWidth) {
-      for (let y = 0; y < p.height; y += rowHeight) {
-        p.line(x, 0, x, p.height);
-        p.line(0, y, p.width, y);
-        console.log('TEst');
-      }
-    }
-    // Set min and max radius for humans
-    // Human.radiusMinMax=createVector(10,20);
+    p.createCanvas(710, 710);
+    p.angleMode(p.DEGREES);
+    p.background(127);
+
+
+    // Setting up the slider for the thickness of the brush
+    // brushSizeSlider = p.createButton('Brush Size Slider');
+    // sizeSlider = p.createSlider(1, 32, 4, 0.1);
   };
 
   p.draw = () => {
-    // drawGrid(p);
+    p.translate(p.width / 2, p.height / 2);
+
+    if (p.mouseX > 0 && p.mouseX < p.width && p.mouseY > 0 && p.mouseY < p.height) {
+      const mx = p.mouseX - p.width / 2;
+      const my = p.mouseY - p.height / 2;
+      const pmx = p.pmouseX - p.width / 2;
+      const pmy = p.pmouseY - p.height / 2;
+
+      if (p.mouseIsPressed) {
+        for (let i = 0; i < symmetry; i++) {
+          p.rotate(angle);
+          // const sw = sizeSlider.value();
+          p.strokeWeight(5);
+          p.line(mx, my, pmx, pmy);
+          p.push();
+          p.scale(1, -1);
+          p.line(mx, my, pmx, pmy);
+          p.pop();
+        }
+      }
+    }
   };
 };
 
 export default new p5(sketch);
-
-/**
- * Draw the grid in the background
- */
-function drawGrid(p: p5) {
-  // console.log(p)
-  // p.stroke(80, 150, 50);
-  // p.strokeWeight(1)
-  // Draw Grid
-  for (let x: number = colWidth; x < p.width; x += colWidth) {
-    for (let y: number = 0; y < p.height; y += rowHeight) {
-      // p.stroke(80, 150, 50);
-      p.stroke(52, 235, 100);
-      p.strokeWeight(1);
-      p.line(x, 0, x, p.height);
-      p.line(0, y, p.width, y);
-      let currentGridCell = x / colWidth - 1;
-      console.log(currentGridCell);
-      console.log(humanKey[currentGridCell]);
-
-      let numArray: number = humanKey[x / colWidth - 1 + y / rowHeight].length;
-      // tempArray.forEach(function (indexValue) {
-      //     if (molecules[indexValue].intersecting == true) {
-      //         intersectCount++
-      //     }
-      // })
-      if (numArray === 0) {
-        // numArray = ""
-      }
-      p.noStroke();
-      p.fill(255, 255, 255, 255);
-      p.textSize(16);
-      p.textAlign(p.RIGHT);
-      // text(numArray, x - 5, y + 20);
-
-      // fill(255, 50, 0, 150);
-      // text(intersectCount, j * gridWidth + gridWidth - 5, i * gridHeight + gridHeight - 5);
-    }
-  }
-}
