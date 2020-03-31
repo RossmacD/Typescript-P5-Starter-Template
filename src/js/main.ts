3f413b672ecd26868669f3162c9fb283960c87df
Ross MacDonald <42966821+RossmacD@users.noreply.github.com>
2020-03-31 01:55:03 +0100

    Added global p5 types

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,22 +1,22 @@
 // Import CSS for webpack - DOsnt work?
 import '../css/myStyle.css';
-import * as p5 from 'p5';
+import Human from './classes/Human'
+
 
 /**
  *  Create a p5 sketch
  * @param p - an isntance of p5
  */
-let sketch = (p: p5) => {
-  /**
-   *P5 Setup
-   */
-  p.setup = () => {
-    p.createCanvas(p.windowWidth, p.windowHeight, p.WEBGL);
-    p.background(0);
-    p.noLoop();
+function setup(){
+    createCanvas(windowWidth, windowHeight, WEBGL);
+    background(0);
+    noLoop();
+    //Set min and max radius for humans
+    Human.radiusMinMax=createVector(10,20);
   };
 
-  p.draw = () => {};
+function draw(){
+    
 };
 
-new p5(sketch);
+
