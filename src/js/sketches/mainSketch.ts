673e1ac1c28a6239ed389da44ed83508d0cde527
Ross MacDonald <42966821+RossmacD@users.noreply.github.com>
2020-04-01 19:47:20 +0100

    Fixed Background

--- src/js/sketches/mainSketch.ts
+++ src/js/sketches/mainSketch.ts
@@ -1,9 +1,18 @@
+/*
+ * Imports
+ */
 import * as p5 from 'p5';
 import PerlinBrush from '../classes/PerlinBrush';
-// Symmetry corresponding to the number of reflections. Change the number for different number of reflections
+
+/*
+ * Global Variables
+ */
+// Number of reflections
 const symmetry: number = 8;
 const angle: number = 360 / symmetry;
+// The randomised brush
 let brush: PerlinBrush;
+
 /*
  * P5 Sketch
  */
@@ -12,29 +21,41 @@ const sketch = (p: p5) => {
    * P5 Setup
    */
   p.setup = () => {
-    const canvasSize=p.windowHeight<p.windowWidth?p.windowHeight:p.windowWidth;
+    // Set canvas size to be asquare which will fit the page exactly
+    const canvasSize = p.windowHeight < p.windowWidth ? p.windowHeight : p.windowWidth;
     p.createCanvas(canvasSize,canvasSize);
+    // Tell p5 to use angles instead of radians
     p.angleMode(p.DEGREES);
-    p.background(0);
+    // Set the background
+    p.background(p.color('#080808'));
+    // Anti Alias the shapes
+    p.smooth();
+    // Create a brush from the PerlinBrush class
     brush = new PerlinBrush(p);
+
   };
 
   /*
    * P5 Draw
    */
   p.draw = () => {
-    p.background(p.color(0,0,0,10))
+    p.noStroke();
+    // Blank the canvas by setting teh background, lower opacity will leave a trailing effect
+    p.background(0,0,0,15)
+    // p.filter(p.ERODE);
+    // p.filter( p.BLUR );
+    // for(let i =0;i<p.width;i++){
+    //   for (let j = 0; j < p.height; j++) {
+    //   }
+    // }
     p.translate(p.width / 2, p.height / 2);
     const mx = brush.x - p.width / 2;
     const my = brush.y - p.height / 2;
     const pmx = brush.px - p.width / 2;
     const pmy = brush.py - p.height / 2;
 
-    // if (p.mouseIsPressed) {
     for (let i = 0; i < symmetry; i++) {
       p.rotate(angle);
-      // const sw = sizeSlider.value();
-      // p.strokeWeight(5);
       p.strokeWeight(5);
       p.stroke(200, brush.x, brush.y);
       p.line(mx, my, pmx, pmy);
@@ -43,8 +64,7 @@ const sketch = (p: p5) => {
       p.line(mx, my, pmx, pmy);
       p.pop();
     }
-    //   }
-    // }
+    // Move the brush
     brush.step(p);
     // brush.display(p);
   };
